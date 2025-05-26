56d466a1d57f62a72fa94703ace1b67469c20489
DeemaRabee <deemarabee2003@gmail.com>
2025-05-26 19:51:47 +0300

    Update uploadOfficialDocument.js

--- middleware/uploadOfficialDocument.js
+++ middleware/uploadOfficialDocument.js
@@ -1,5 +1,4 @@
 
-// 📂 middleware/uploadOfficialDocument.js
 const multer = require('multer');
 const path = require('path');
 
@@ -22,37 +21,3 @@ const officialStorage = multer.diskStorage({
 const uploadOfficialDocument = multer({ storage: officialStorage, fileFilter: pdfFilter });
 
 module.exports = uploadOfficialDocument;
-
-/*
-const multer = require('multer');
-const path = require('path');
-const officialStorage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/official/'),
-    filename: (req, file, cb) => cb(null, `${req.params.id}-official-${Date.now()}${path.extname(file.originalname)}`)
-  });
-  
-  const uploadOfficialDocument = multer({ storage: officialStorage, fileFilter: pdfFilter });
-  module.exports = uploadOfficialDocument;*/
-/*const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/officialDocuments/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${req.params.id}-official-${Date.now()}${path.extname(file.originalname)}`);
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'application/pdf') {
-    cb(null, true);
-  } else {
-    cb(new Error('Only PDF files are allowed'), false);
-  }
-};
-
-const uploadOfficialDocument = multer({ storage, fileFilter });
-
-module.exports = uploadOfficialDocument;*/

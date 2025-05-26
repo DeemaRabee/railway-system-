c0640e648ffa268c13d63708854b7b7cda135a7f
DeemaRabee <deemarabee2003@gmail.com>
2025-05-26 19:51:21 +0300

    Update uploadCompanyProfilePicture.js

--- middleware/uploadCompanyProfilePicture.js
+++ middleware/uploadCompanyProfilePicture.js
@@ -1,4 +1,4 @@
-// 📂 middleware/uploadCompanyProfilePicture.js
+
 const multer = require('multer');
 const path = require('path');
 const companyProfileStorage = multer.diskStorage({
@@ -18,27 +18,3 @@ module.exports = uploadCompanyProfilePicture;
 
 
 
-/*const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/profilePictures/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${req.user.id}-${Date.now()}${path.extname(file.originalname)}`);
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error('Only JPEG, JPG, or PNG images are allowed'), false);
-  }
-};
-
-const uploadCompanyProfilePicture = multer({ storage, fileFilter });
-
-module.exports = uploadCompanyProfilePicture;*/

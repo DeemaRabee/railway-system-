a2f370675dc80918c82755c081403e823e11bc24
DeemaRabee <deemarabee2003@gmail.com>
2025-05-26 18:51:13 +0300

    Update Application.js

--- models/Application.js
+++ models/Application.js
@@ -1,4 +1,4 @@
-//console.log("application.js");
+
 const mongoose = require('mongoose');
 
 const applicationSchema = new mongoose.Schema({
@@ -54,49 +54,3 @@ applicationSchema.pre('save', function (next) {
 const Application = mongoose.model('Application', applicationSchema);
 
 module.exports = Application;
-
-/*const mongoose = require('mongoose');
-
-const applicationSchema = new mongoose.Schema({
-  student: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Student',
-    required: true
-  },
-  trainingPost: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'TrainingPost',
-    required: true
-  },
-  cv: {
-    type: String,
-    required: [true, 'CV is required']
-  },
-  status: {
-    type: String,
-    enum: ['UNDER_REVIEW', 'APPROVED', 'REJECTED'],
-    default: 'UNDER_REVIEW'
-  },
-  selectedByStudent: {
-    type: Boolean,
-    default: false
-  },
-  officialDocument: {
-    type: String
-  },
-  approvalFiles: [String],
-  activityReports: [String],
-  finalReport: String,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: Date
-});
-
-// Prevent duplicate applications
-applicationSchema.index({ student: 1, trainingPost: 1 }, { unique: true });
-
-const Application = mongoose.model('Application', applicationSchema);
-
-module.exports = Application;*/
